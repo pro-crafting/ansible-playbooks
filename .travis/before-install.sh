390aaa6da23961fc98632862045fed265335d7ed
Martin Panzer <postremus1996@googlemail.com>
2018-08-02 23:30:35 +0200

    slashes

--- .travis/before-install.sh
+++ .travis/before-install.sh
@@ -5,6 +5,6 @@ sudo apt-get update
 sudo apt-get install -y ansible
 export ANSIBLE_HOST_KEY_CHECKING=false
 
-openssl aes-256-cbc -K $encrypted_cfcca50f44cd_key -iv $encrypted_cfcca50f44cd_iv -in .travis\id_rsa_docker.enc -out .travis\id_rsa_docker -d
-chmod 0400 .travis\id_rsa_docker
-ssh-add .travis\id_rsa_docker
+openssl aes-256-cbc -K $encrypted_cfcca50f44cd_key -iv $encrypted_cfcca50f44cd_iv -in .travis/id_rsa_docker.enc -out .travis/id_rsa_docker -d
+chmod 0400 .travis/id_rsa_docker
+ssh-add .travis/id_rsa_docker
