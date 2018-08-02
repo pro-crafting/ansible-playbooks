600eccb5934653c820081537b4926ca34c5e7694
Martin Panzer <postremus1996@googlemail.com>
2018-08-02 21:53:12 +0200

    disable checking for cron job for now

--- .travis/cron-dev.sh
+++ .travis/cron-dev.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_EVENT_TYPE" = 'cron' ]; then
+#if [ "$TRAVIS_EVENT_TYPE" = 'cron' ]; then
     ansible-playbook -i inventory/staging common-docker-swarm.yml --tags deploy,rjc
-fi
+#fi
